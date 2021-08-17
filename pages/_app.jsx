5ca370982f9134efe013692ef1592341f1ccd772
jepicode <jepi.usuluddin@gmail.com>
2021-08-17 14:44:13 +0700

    First Initialize

---
+++ pages/_app.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
+  prefix: 'tw-',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: (theme) => ({
+        ...theme('colors'),
+        'main-background': '#F4F1EC'
+      })
+    },
+    fontFamily: {
+      'open-sans': ['"Open Sans"', 'sans-serif'],
+      sacramento: ['Sacramento', 'sans-serif']
+    },
+    letterSpacing: {
+      widest: '0.6em'
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
