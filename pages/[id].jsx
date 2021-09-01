bda81b32165262bcb41c24982a7fc316e7680e55
jepicode <jepi.usuluddin@gmail.com>
2021-09-02 00:56:29 +0700

    Hide Rekening if Ibu Ibu

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -412,30 +412,32 @@ export default function Home() {
             <div className='tw-w-full tw-border-t tw-border-black' />
           </div>
 
-          <div className='tw-w-full tw-px-4 sm:tw-p-0 sm:tw-w-1/2 tw-my-12 tw-mx-auto tw-text-center sm:tw-text-lg'>
-            <div className='tw-my-4'>
-              Tanpa mengurangi rasa hormat, bagi keluarga, sahabat dan rekan yang ingin
-              memberikan tanda kasih untuk kami, dapat melalui :
-            </div>
-            <div className='tw-mt-8 tw-flex'>
-              <div className='tw-w-1/2'>
-                <Image src='/bank/bca.png' width='150' height='48' />
-                <button type='button' className='tw-text-bold tw-flex tw-justify-center tw-w-full tw-items-center' title='Copy' onClick={() => navigator.clipboard.writeText('4660058899')}>
-                  4660058899
-                  <FaCopy className='tw-ml-2 tw-fill-current tw-text-gray-600 tw-text-sm' />
-                </button>
-                <div className='tw-text-sm'>a/n Reza Ayu Pertiwi</div>
+          {!ShowTitle && (
+            <div className='tw-w-full tw-px-4 sm:tw-p-0 sm:tw-w-1/2 tw-my-12 tw-mx-auto tw-text-center sm:tw-text-lg'>
+              <div className='tw-my-4'>
+                Tanpa mengurangi rasa hormat, bagi keluarga, sahabat dan rekan yang ingin
+                memberikan tanda kasih untuk kami, dapat melalui :
               </div>
-              <div className='tw-w-1/2'>
-                <Image src='/bank/mandiri.png' width='150' height='48' />
-                <button type='button' className='tw-text-bold tw-flex tw-justify-center tw-w-full tw-items-center' title='Copy' onClick={() => navigator.clipboard.writeText('1180010569068')}>
-                  1180010569068
-                  <FaCopy className='tw-ml-2 tw-fill-current tw-text-gray-600 tw-text-sm' />
-                </button>
-                <div className='tw-text-sm'>a/n Jepi Usuluddin</div>
+              <div className='tw-mt-8 tw-flex'>
+                <div className='tw-w-1/2'>
+                  <Image src='/bank/bca.png' width='150' height='48' />
+                  <button type='button' className='tw-text-bold tw-flex tw-justify-center tw-w-full tw-items-center' title='Copy' onClick={() => navigator.clipboard.writeText('4660058899')}>
+                    4660058899
+                    <FaCopy className='tw-ml-2 tw-fill-current tw-text-gray-600 tw-text-sm' />
+                  </button>
+                  <div className='tw-text-sm'>a/n Reza Ayu Pertiwi</div>
+                </div>
+                <div className='tw-w-1/2'>
+                  <Image src='/bank/mandiri.png' width='150' height='48' />
+                  <button type='button' className='tw-text-bold tw-flex tw-justify-center tw-w-full tw-items-center' title='Copy' onClick={() => navigator.clipboard.writeText('1180010569068')}>
+                    1180010569068
+                    <FaCopy className='tw-ml-2 tw-fill-current tw-text-gray-600 tw-text-sm' />
+                  </button>
+                  <div className='tw-text-sm'>a/n Jepi Usuluddin</div>
+                </div>
               </div>
             </div>
-          </div>
+          )}
 
           <div className='tw-relative'>
             <div className='tw-absolute tw-w-full tw-left-0 tw-overflow-hidden tw-z-10'>
