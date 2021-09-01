3dac7d6e8652c63317c76fc092965e4fee94ff70
jepicode <jepi.usuluddin@gmail.com>
2021-09-02 00:43:32 +0700

    Tracing

--- pages/api/greetings.js
+++ pages/api/greetings.js
@@ -31,6 +31,40 @@
 
 // export default connectDB(handler);
 
-const handler = async (req, res) => res.status(200);
+import dbConnect from '../../lib/dbConnect';
+import Greeting from '../../models/greeting';
 
-export default handler;
+export default async function handler(req, res) {
+  const { method } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case 'GET':
+      try {
+        const listGreetings = await Greeting.find({});
+        res.status(200).json({ success: true, data: listGreetings });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case 'POST':
+      try {
+        const { name, greetings, isWillCome } = req.body;
+
+        const greetingData = new Greeting({
+          name,
+          greetings,
+          isWillCome
+        });
+        await greetingData.save();
+        res.status(200).json({ status: true, message: 'Succes' });
+      } catch (err) {
+        res.status(500).json({ status: false, message: 'Failed Save' });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
