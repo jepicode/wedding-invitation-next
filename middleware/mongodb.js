a715c1535b8efca3be05a2fc7a08fdd029f949f1
jepicode <jepi.usuluddin@gmail.com>
2021-09-02 00:15:59 +0700

    Tracing

--- middleware/mongodb.js
+++ middleware/mongodb.js
@@ -6,7 +6,7 @@ const connectDB = (handler) => async (req, res) => {
     return handler(req, res);
   }
   // Use new db connection
-  await mongoose.connect(process.env.mongodburl, (err) => {
+  await mongoose.connect('mongodb+srv://admin:T1oj0!T2Y^wMkfPk@cluster0.mo5pt.mongodb.net/greetings', (err) => {
     if (err) throw err;
     console.log('Connected to DB');
   });
